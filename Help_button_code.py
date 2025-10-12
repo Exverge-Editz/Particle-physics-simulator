52a16f876f5e8bb06c578e00b18707cac925e29e
Callum Fromant <callumfromant@gmail.com>
2025-10-12 20:20:16 +0100

    Update Help_button_code.py
    .fixes an issue with the help button not staying on the screen after you have pressed it.
    .allows the user to remove the help menu by pressing the button a second time.

--- Help_button_code.py
+++ Help_button_code.py
@@ -1,26 +1,26 @@
-import pygame #type: ignore
+import pygame
 
 class Help_button:
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        self.clicked = False #states if the button has been clicked
+        self.clicked = False
+        self.menu_visible = False  #visibilty flag
 
     def draw(self, surface):
         action = False
         LEFT_MOUSE_BUTTON = 0
-
         pos = pygame.mouse.get_pos()
 
         if self.rect.collidepoint(pos):
-            #allows you to click the button
-            if pygame.mouse.get_pressed()[LEFT_MOUSE_BUTTON] == True and self.clicked == False:
+            if pygame.mouse.get_pressed()[LEFT_MOUSE_BUTTON] == True and not self.clicked:
                 self.clicked = True
+                self.menu_visible = not self.menu_visible  #Toggle visibility
                 action = True
-        #if pygame.mouse.get_pressed()[LEFT_MOUSE_BUTTON] == False : #button hasn't been clicked
+
+        if pygame.mouse.get_pressed()[LEFT_MOUSE_BUTTON] == False:
             self.clicked = False
 
         surface.blit(self.image, (self.rect.x, self.rect.y))
-
         return action
